c4099591324b6dc5b2d1238e842f545d2f096460
Ronny Hsieh <1020ronny@gmail.com>
2020-05-20 14:46:17 +0800

    login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,20 @@ import React from "react";
 function Login() {
 	return (
 		<div className="container">
-			<h1>Login</h1>
+			<form>
+				<div className="form-group">
+					<label htmlFor="emailOrPhone">請輸入信箱或手機</label>
+					<input type="text" className="form-control" id="emailOrPhone" required />
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">密碼</label>
+					<input type="password" className="form-control" id="password" required />
+				</div>
+
+				<button type="submit" className="btn btn-primary">
+					登入
+				</button>
+			</form>
 		</div>
 	);
 }
